6a7c582c840083255ea133c57e971a3bb64b5b96
Tigabu <tigabu.m@productera.io>
2023-06-30 00:45:48 +0300

    refact: format code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Component/Header";
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>Contact</h1>
       <h1>Menu</h1>
       <h1>Body</h1>
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-return(
+  return (
     <div>
-        <p>Welcome to Possible, a cleaning service provider. <br/>
-            We are best in this service for long time.
-        </p>
-        <a>Support</a> { " "}
-        <a>Contact Us</a>{" "}
-        <a>Lan: Eng</a>{" "}
-        <a>f</a>{" "}
-        <a>t</a>{" "}
-        <a>in</a>{" "}
-        <a>IG</a>{" "}
+      <p>
+        Welcome to Possible, a cleaning service provider. <br />
+        We are best in this service for long time.
+      </p>
+      <a>Support</a> <a>Contact Us</a> <a>Lan: Eng</a> <a>f</a> <a>t</a>{" "}
+      <a>in</a> <a>IG</a>{" "}
     </div>
-)
-}
+  );
+};
 
-export default Header
+export default Header;
